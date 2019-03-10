8de4d0699a1c2725165c630722745c791ae69c00
lzt <1071453851@qq.com>
2019-03-10 17:41:35 +0800

    cglib代理代码实现

--- src/code/mode/proxy/ProxyTestMain.java
+++ src/code/mode/proxy/ProxyTestMain.java
@@ -4,6 +4,7 @@ import mode.proxy.interfaceImplPgk.Dog;
 import mode.proxy.interfacePgk.Animal;
 import mode.proxy.proxyClassPkg.DogDynamicProxy;
 import mode.proxy.proxyClassPkg.DogStaticProxy;
+import mode.proxy.proxyClassPkg.cglib.CglibProxy;
 
 /**
  * Created by Lei on 2019/3/9.
@@ -12,9 +13,16 @@ public class ProxyTestMain {
 
     public static void main(String[] args) {
 //        animalStaticProxyMethod();
-        animalDynamicProxyMethod();
+//        animalDynamicProxyMethod();
+        animalCglibProxyMethod();
     }
 
+    private static void animalCglibProxyMethod() {
+        CglibProxy dogCglibProxy = new CglibProxy();
+        Dog animal = (Dog) dogCglibProxy.getInstance();
+        animal.eat();
+
+    }
     private static void animalDynamicProxyMethod() {
         DogDynamicProxy dogDynamicProxy = new DogDynamicProxy();
         Animal animal = dogDynamicProxy.getInstance();
